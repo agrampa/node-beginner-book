f37908e8f181c3046d594fbbec2078b178e39572
Allie Grampa <agrampa@yahoo.com>
2017-04-30 16:20:05 -0700

    completed tutorial

--- server.js
+++ server.js
@@ -8,8 +8,20 @@ function start(route, handle) {
 function onRequest(request, response) {
     let pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + 'received.');
+    route(handle, pathname, response, request);
     
-    route(handle, pathname, response);
+    // request.setEncoding('utf8');
+    // 
+    // request.addListener('data', function(postDataChunk) {
+    //   postData += postDataChunk;
+    //   console.log('Received POST data chunk "' + postDataChunk + '".');
+    // });
+    // 
+    // request.addListener('end', function() {
+    //   route(handle, pathname, response, postData);
+    // });
+    
+    // route(handle, pathname, response);
     
     // response.writeHead(200, {'Content-Type': 'text/plain'});
     // let content = route(handle, pathname);
